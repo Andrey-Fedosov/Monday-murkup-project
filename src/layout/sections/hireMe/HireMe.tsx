3f2be74eb281601a2b0288231b8a6f530ff1fa91
Andrey-Fedosov <andrefedosov@gmail.com>
2023-07-23 11:06:38 +0300

    add hire section in site structure

---
+++ src/layout/sections/hireMe/HireMe.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { FlexWrapper } from '../../../components/flexWrapper/FlexWrapper';
+import styled from 'styled-components';
+import { SectionTitle } from '../../../components/sectionTitle/SectionTitle';
+
+export function HireMe() {
+  return (
+    <StyledHireMe>
+      <FlexWrapper direction="column" justify="center" align="center">
+        <SectionTitle titleValue="I Am Available For Freelance" />
+        <input type="button" value="HIRE ME" />
+      </FlexWrapper>
+    </StyledHireMe>
+  );
+}
+
+const StyledHireMe = styled.div`
+  min-height: 100vh;
+  background-color: #fdadad;
+`;
